02694cb477bc2b207385419007c0c50b930858b0
Sweta <spmac@swetas-mbp.lan>
2023-06-21 15:54:49 -0400

    create header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,20 @@
+import { NavLink, Link } from "react-router-dom";
+import logo from "../../assets/logo.png"
+import SearchBar from "../SearchBar/SearchBar";
 import "./Header.scss";
 
 const Header = () => {
     return (
-        <div>
+        <div className="header">
+            <div className="header__container">
+            {/* <img className="header__logo" src={logo} alt="water logo" /> */}
+            <Link to="/assets" className="header__link"> 
+            <h1 className="header__title">ClearWater</h1>
+            </ Link>
+            </div>
             
+
+            {/* <SearchBar /> */}
         </div>
     );
 };
---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,13 @@
+import search from "../../assets/search.svg";
+import "./SearchBar.scss";
+
+const SearchBar = () => {
+    return (
+        <div className="search">
+            <input type="text" placeholder="Search.." className="search__input" />
+            <img src={search} alt="search image" className="search__img"/>
+        </div>
+    );
+};
+
+export default SearchBar;

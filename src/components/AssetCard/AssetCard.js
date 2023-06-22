6b809f0ec2e3a1cd6cdfb0d6c4ee1514cedd6314
Sweta <spmac@swetas-mbp.lan>
2023-06-22 01:46:38 -0400

    add content to the asset details component and add mobile styling

--- src/components/AssetCard/AssetCard.js
+++ src/components/AssetCard/AssetCard.js
@@ -1,33 +1,36 @@
 import "./AssetCard.scss";
 
 const AssetCard = ({ name, type, assetId, location, lastMaintenancedate }) => {
-    
-    return (
-        <div className="asset">
-            <div className="asset__field">
-                <label className="asset__label">NAME:</label>
-                <p className="asset__text">{name}</p>
-            </div>
-            <div className="asset__field-container">
-            <div className="asset__field">
-                <label className="asset__label">TYPE:</label>
-                <p className="asset__text">{type}</p>
-            </div>
-            <div className="asset__field">
-                <label className="asset__label">ASSET ID:</label>
-                <p className="asset__text">{assetId}</p>
-            </div>
-            </div>
-            <div className="asset__field">
-                <label className="asset__label">LOCATION:</label>
-                <p className="asset__text">{location}</p>
-            </div>
-            <div className="asset__field">
-                <label className="asset__label">LAST MAINTENANCE DATE:</label>
-                <p className="asset__text">{lastMaintenancedate}</p>
-            </div>
+  return (
+    <div className="asset">
+      <div className="asset__left">
+        <div className="asset__field">
+          <label className="asset__label">NAME:</label>
+          <p className="asset__text">{name}</p>
         </div>
-    );
+        <div className="asset__field-container">
+          <div className="asset__field">
+            <label className="asset__label">TYPE:</label>
+            <p className="asset__text">{type}</p>
+          </div>
+          <div className="asset__field">
+            <label className="asset__label">ASSET ID:</label>
+            <p className="asset__text">{assetId}</p>
+          </div>
+        </div>
+      </div>
+      <div  className="asset__right">
+      <div className="asset__field">
+        <label className="asset__label">LOCATION:</label>
+        <p className="asset__text">{location}</p>
+      </div>
+      <div className="asset__field">
+        <label className="asset__label">LAST MAINTENANCE DATE:</label>
+        <p className="asset__text">{lastMaintenancedate}</p>
+      </div>
+      </div>
+    </div>
+  );
 };
 
 export default AssetCard;
--- src/components/AssetDetails/AssetDetails.js
+++ src/components/AssetDetails/AssetDetails.js
@@ -1,21 +1,47 @@
+import { date } from "../../utils";
 import "./AssetDetails.scss";
 
 const AssetDetails = ({ currAsset }) => {
-    return (
-        <div className="assetdetails">
-            <div className="assetdetails__left">
-          <div className="assetdetails__description">
-            <h3 className="assetdetails__label">ITEM DESCRIPTION:</h3>
-            <p className="assetdetails__text">{currAsset.assetDescription}</p>
-          </div>
-          <div className="assetdetails__category">
-            <h3 className="assetdetails__label">CATEGORY:</h3>
-            <p className="assetdetails__text">{currAsset.assetType}</p>
-          </div>
+  return (
+    <div className="assetdetails">
+      <div className="assetdetails__left">
+        <div className="assetdetails__field">
+          <label className="asset__label">ITEM DESCRIPTION:</label>
+          <p className="assetdetails__text">{currAsset.assetDescription}</p>
         </div>
-            
+        <div className="assetdetails__field">
+          <label className="asset__label">MANUFACTURER:</label>
+          <p className="assetdetails__text">{currAsset.manufacturer}</p>
         </div>
-    );
+        <div className="assetdetails__field">
+          <label className="asset__label">SERIAL #:</label>
+          <p className="assetdetails__text">{currAsset.serialNumber}</p>
+        </div>
+        <div className="assetdetails__field">
+          <label className="asset__label">STATUS:</label>
+          <p className="assetdetails__text">{currAsset.status}</p>
+        </div>
+      </div>
+      <div className="assetdetails__right">
+        <div className="assetdetails__field">
+          <label className="asset__label">NEXT MAINTENANCE DATE:</label>
+          <p className="assetdetails__text">{date(currAsset.nextMaintenanceDate)}</p>
+        </div>
+        <div className="assetdetails__field">
+          <label className="asset__label">MAINTENANCE INTERVAL:</label>
+          <p className="assetdetails__text">{currAsset.maintenanceInterval}</p>
+        </div>
+        <div className="assetdetails__field">
+          <label className="asset__label">PURCHASE DATE:</label>
+          <p className="assetdetails__text">{date(currAsset.purchaseDate)}</p>
+        </div>
+        <div className="assetdetails__field">
+          <label className="asset__label">WARRANTY EXPIRATION DATE:</label>
+          <p className="assetdetails__text">{date(currAsset.warrantyExpirationDate)}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AssetDetails;
